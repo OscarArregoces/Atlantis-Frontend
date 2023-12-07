b60e884704026ce119d9d2ee77a1a2405d968f4e
Oscar Arregoces <ingoscararregoces@gmail.com>
2023-12-07 17:15:20 -0500

    Readme media

--- src/layouts/PrivateLayout.jsx
+++ src/layouts/PrivateLayout.jsx
@@ -1,24 +1,9 @@
 import { Navigate, Outlet } from "react-router-dom"
 import { Navbar } from "../components/private/Navbar"
-import { useEffect, useState } from "react";
-import { useAxios } from "../utils/axios.instance";
-
+import { useState } from "react";
 
 export const PrivateLayout = () => {
   const [isValid, setIsValid] = useState(true);
-  // useEffect(() => {
-  //   async function getCategory() {
-  //     const { data } = await useAxios.get('/category');
-  //     if (data.error) {
-  //       return setIsValid(false);
-  //     }
-  //     if (data.data === 'Invalid Token') {
-  //       return setIsValid(false)
-  //     }
-  //     return setIsValid(true)
-  //   };
-  //   getCategory();
-  // }, []);
 
   if (!isValid) {
     return <Navigate to="/login" replace />;
